0b038d5e540a242fba9296a0ceb5aee54c89fafb
George-hajjirge <Ramesh.george@techdenali.com>
2026-07-13 18:59:32 +0530

    solved build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 
-import { defineConfig } from 'vite';
+import { defineConfig } from "vitest/config";
 import react from '@vitejs/plugin-react';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import path from 'path';
